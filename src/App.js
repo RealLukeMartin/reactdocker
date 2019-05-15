154f23e36d1490004a83342fba3461c7535a2988
RealLukeMartin <lukemartinwebdev@gmail.com>
2019-05-14 23:22:07 -0500

    deploy: revert deploy-test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          What Up A!!!
+          React on Docker
         </p>
         <a
           className="App-link"
